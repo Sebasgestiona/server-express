12120aa86d68c30fb5ba7cb1f1346e9253401401
Sebasgestiona <159690861+Sebasgestiona@users.noreply.github.com>
2024-02-18 19:00:17 +0100

    Add prettier

---
+++ src/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+//PAth se utiliza para no tener que crear rutas absolutas.
+const path = require('path');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  //creamos una ruta simplificada para no tener que crear rutas absolutas en el futuro.
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.get('/api/hello', (req, res) => {
+  res.json({ message: 'hello por GET' });
+});
+
+app.listen(3000, () => {
+  //mensaje que queremos que aparezca cada vez que se reinicia el servidor.
+  console.log('Server listening on port 3000');
+});
+
+//Instalamos un nuevo modulo para que el servidor se reinicie solo cada vez que hagamos una modificación,
+//npm i nodemon -d (-d para que sea una dependencia de desarrollo)
+//Añadimos en package.json, dentro de "script" añadimos: "start": nodemon app.js" (nombre aplicación, app.js)
+//Para iniciar el nuevo modulo, lo hacemos en la consola con la siguiente linea: npm run start
